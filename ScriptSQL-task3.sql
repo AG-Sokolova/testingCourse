ebd67b25df2d481d2bb07074f9d546e283935e4d
Anastasia <34191733+AnastasiaSokolov@users.noreply.github.com>
2022-01-09 18:08:48 +0400

    Update ScriptSQL-task3.sql

--- ScriptSQL-task3.sql
+++ ScriptSQL-task3.sql
@@ -1,99 +1,95 @@
 -- 1.Вывести всех работников чьи зарплаты есть в базе, вместе с зарплатами.
-select employee_name, monthly_salary from employees e 
-	inner join employee_salary es 
-		on e.id = es.employee_id 
-	inner join salary s
-		on es.salary_id = s.id;
+	select e.employee_name, s.monthly_salary from employee_salary es 
+		join employees e on es.employee_id = e.id
+		join salary s on es.salary_id = s.id 
+	;
 
 -- 2.Вывести всех работников у которых ЗП меньше 2000.
-select employee_name, monthly_salary from employees e 
-	inner join employee_salary es 
-		on e.id = es.employee_id 
-	inner join salary s 
-		on es.salary_id =s.id 
-	where s.monthly_salary < '2000';
+	select e.employee_name, s.monthly_salary from employee_salary es 
+		join employees e on es.employee_id = e.id 
+		join salary s on es.salary_id = s.id
+		where s.monthly_salary < '2000'
+	;
 	
 -- 3.Вывести все зарплатные позиции, но работник по ним не назначен. (ЗП есть, но не понятно кто её получает.)
-select es.employee_id, s.monthly_salary from employee_salary es 
-	full outer join employees e 
-		on es.employee_id = e.id 
-	full outer join salary s 
-		on es.salary_id = s.id 
-	where employee_name is null;
+	select /*es.employee_id,*/ s.monthly_salary from employee_salary es 
+		inner join salary s on es.salary_id = s.id 
+		left join employees e on es.employee_id = e.id
+		where e.id is null
+	;
 	
 -- 4.Вывести все зарплатные позиции  меньше 2000 но работник по ним не назначен. (ЗП есть, но не понятно кто её получает.)
-select es.employee_id, s.monthly_salary from employee_salary es 
-	full outer join employees e 
-		on es.employee_id = e.id 
-	full outer join salary s 
-		on es.salary_id = s.id
-	where employee_name is null and s.monthly_salary < '2000';
+	select /*es.employee_id,*/ s.monthly_salary from employee_salary es 
+		inner join salary s on es.salary_id = s.id 
+		left join employees e on es.employee_id = e.id
+		where e.id is null and s.monthly_salary < '2000'
+	;
 	
 -- 5.Найти всех работников кому не начислена ЗП.
-select e.employee_name from employee_salary es 
-	full outer join employees e 
-		on es.employee_id = e.id
-	where es.salary_id is null;
-	
+	select e.employee_name from employee_salary es 
+		full outer join employees e on es.employee_id = e.id
+		where salary_id is null
+	;
 	
 -- 6.Вывести всех работников с названиями их должности.
-select e.employee_name, r.role_name from roles_employee re 
-	inner join employees e 
-		on re.employee_id = e.id 
-	inner join roles r 
-		on re.role_id = r.id;
+	select e.employee_name, r.role_name from roles_employee re 
+		join employees e on re.employee_id = e.id 
+		join roles r on re.role_id = r.id 
+	;
 	
 -- 7.Вывести имена и должность только Java разработчиков.
-select e.employee_name, r.role_name from roles_employee re 
-	inner join employees e 
-		on re.employee_id = e.id 
-	inner join roles r 
-		on re.role_id = r.id
-	where role_name like '% Java %';
+	select e.employee_name, r.role_name from roles_employee re 
+		join employees e on re.employee_id = e.id 
+		join roles r on re.role_id = r.id 
+		where r.role_name like '% Java %'
+	;
 	
 -- 8.Вывести имена и должность только Python разработчиков.
-select e.employee_name, r.role_name from roles_employee re 
-	inner join employees e 
-		on re.employee_id = e.id 
-	inner join roles r 
-		on re.role_id = r.id 
-	where r.role_name like '% Python %';
+	select e.employee_name, r.role_name from roles_employee re 
+		join employees e on re.employee_id = e.id 
+		join roles r on re.role_id = r.id 
+		where r.role_name like '% Python %'
+	;
 	
 -- 9.Вывести имена и должность всех QA инженеров.
-select e.employee_name, r.role_name from roles_employee re 
-	inner join employees e 
-		on re.employee_id = e.id 
-	inner join roles r 
-		on re.role_id = r.id 
-	where r.role_name like '% QA engineer';
+	select e.employee_name, r.role_name from roles_employee re 
+		join employees e on re.employee_id = e.id 
+		join roles r on re.role_id = r.id 
+		where r.role_name like '% QA %'
+	;
 	
 -- 10.Вывести имена и должность ручных QA инженеров.
-select e.employee_name, r.role_name from roles_employee re 
-	inner join employees e 
-		on re.employee_id = e.id 
-	inner join roles r 
-		on re.role_id = r.id 
-	where r.role_name like '% Manual QA engineer';
+	select e.employee_name, r.role_name from roles_employee re 
+		join employees e on re.employee_id = e.id 
+		join roles r on re.role_id = r.id 
+		where r.role_name like '% Manual QA %'
+	;
 	
 -- 11.Вывести имена и должность автоматизаторов QA
-select e.employee_name, r.role_name from roles_employee re 
-	inner join employees e 
-		on re.employee_id = e.id 
-	inner join roles r 
-		on re.role_id = r.id 
-	where r.role_name like '% Automation QA %';
+	select e.employee_name, r.role_name from roles_employee re 
+		join employees e on re.employee_id = e.id 
+		join roles r on re.role_id = r.id 
+		where r.role_name like '% Automation QA %'
+	;
 	
 -- 12.Вывести имена и зарплаты Junior специалистов 
-select e.employee_name, s.monthly_salary, r.role_name from roles_employee re 
-	inner join employees e 
-		on re.employee_id = e.id 
-	inner join roles r 
-		on re.role_id = r.id 
-	right join employee_salary es 
-		on re.employee_id = es.employee_id
-	inner join salary s 
-		on es.salary_id = s.id 
-	where r.role_name like 'Junior %';
+	select e.employee_name,/* r.role_name,*/ s.monthly_salary from employee_salary es 
+		inner join employees e on es.employee_id = e.id 
+		inner join salary s on es.salary_id = s.id 
+		inner join roles_employee re on es.employee_id = re.employee_id 
+		inner join roles r on re.role_id = r.id
+		where r.role_name like 'Junior %'
+	;
+
+	select e.employee_name, s.monthly_salary from employee_salary es 
+		inner join employees e on es.employee_id = e.id 
+		inner join salary s on es.salary_id = s.id 
+		where es.employee_id in 
+			(select employee_id from roles_employee re 
+				inner join roles r on re.role_id = r.id 
+				where r.role_name like 'Junior %'
+			)
+	;
 	
 -- 13.Вывести имена и зарплаты Middle специалистов
 select e.employee_name, s.monthly_salary, r.role_name from roles_employee re 
